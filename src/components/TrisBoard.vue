52869b4ccb778274d8a67c3e022088cae53c2f77
matteo <matteo.razzanelli89@gmail.com>
2023-01-25 16:28:41 +0100

    wip tris board, emit event ok

---
+++ src/components/TrisBoard.vue
@@ -0,0 +1,24 @@
+<template>
+  <button type="button" @click.once="changeStatus()">{{ buttonStatus }}</button>
+</template>
+
+<script>
+  export default {
+    name: 'TrisBoard',
+    data() {
+      return {
+        buttonStatus: 'Boh'
+      }
+    },
+    methods: {
+      changeStatus(){
+        console.log('dentro');
+        this.$emit('changed', this.buttonStatus);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+  <h1>{{ playerX }} VS. {{ playerO }}</h1>
+  <div class="game_table">
+    <!-- Create the board and listen on click events -->
+    <TrisBoard @changed="changedFcn"/>
+  </div>
+</template>
+
+<script>
+import TrisBoard from "@/components/TrisBoard.vue";
+export default {
+  name: 'GameView',
+  components: { TrisBoard },
+  props: ["playerX", "playerO"],
+  methods: {
+    changedFcn(changedVar){
+      console.log('Arrivato ' + changedVar);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
