cdcceddd023d3ad0c6f2efedfcf3a5a2189826c1
matteo <matteo.razzanelli89@gmail.com>
2023-01-26 14:46:15 +0100

    a complete match can be played

--- src/components/TrisBoard.vue
+++ src/components/TrisBoard.vue
@@ -1,24 +1,43 @@
 <template>
-  <button type="button" @click.once="changeStatus()">{{ buttonStatus }}</button>
+  <div v-for="x in 3" :key="x">
+    <div v-for="y in 3" :key="y">
+      <!-- Note that index starts from 1 while boards' first index is 0 -->
+      <!-- Note that with nested v-for and grid index are reversed -->
+      <div class="grid-elem" @click="changeStatus(y,x)">{{ board[y-1][x-1] }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'TrisBoard',
-    data() {
-      return {
-        buttonStatus: 'Boh'
-      }
+    emits: ['changed'],
+    props: ['board', 'hasWinner'],
+    mounted(){
+      console.log(this.board);
     },
     methods: {
-      changeStatus(){
-        console.log('dentro');
-        this.$emit('changed', this.buttonStatus);
+      changeStatus(x,y){
+        if(!this.hasWinner){
+          this.$emit('changed', x,y);
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-
+/* *{border:1px solid blue} */
+  .grid-elem {
+    font-size: 75px;
+    border: 2px solid crimson;
+    background-color: #3a3a3c;
+    color: white;
+    cursor: pointer;
+    width: 90px;
+    height: 90px;
+    margin: 5px;
+    border-radius: 4px;
+    box-shadow: 3px 3px 3px rgb(109, 107, 107);
+  }
 </style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,10 +1,17 @@
 
 
 <template>
-  <h1>{{ playerX }} VS. {{ playerO }}</h1>
+  <h1>{{ playerX }} (X) VS. {{ playerO }} (O)</h1>
   <div class="game_table">
     <!-- Create the board and listen on click events -->
-    <TrisBoard @changed="changedFcn"/>
+    <TrisBoard @changed="changedFcn" :board="board" :hasWinner="hasWinner"/>
+  </div>
+  <div v-if="hasWinner">
+    <h2>Player {{ currentPlayer }} WINS!</h2>
+    <button @click="restartGame">Restart</button>
+  </div>
+  <div v-else>
+    <h2>Next move : {{ currentPlayer }}</h2>
   </div>
 </template>
 
@@ -12,16 +19,91 @@
 import TrisBoard from "@/components/TrisBoard.vue";
 export default {
   name: 'GameView',
+  data() {
+    return {
+      board: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      combinations: [
+        [[0,0],[0,1],[0,2]],
+        [[1,0],[1,1],[1,2]],
+        [[2,0],[2,1],[2,2]],
+        [[0,0],[1,0],[2,0]],
+        [[0,1],[1,1],[2,1]],
+        [[0,2],[1,2],[2,2]],
+        [[0,0],[1,1],[2,2]],
+        [[2,0],[1,1],[0,2]]
+      ],
+      currentPlayer: '',
+      hasWinner: false
+    }
+  },
   components: { TrisBoard },
   props: ["playerX", "playerO"],
+  mounted(){
+    // default playerX is the first to move
+    this.currentPlayer = this.playerX;
+  },
   methods: {
-    changedFcn(changedVar){
-      console.log('Arrivato ' + changedVar);
+    changedFcn(x,y){
+      // Sign the board wrt currentPlayer
+      this.board[x-1][y-1] = this.currentPlayer == this.playerX ? 'X' : 'O';
+      // Check if currentPlayer wins
+      if(this.checkWinner()){
+        this.hasWinner = true;
+      }
+      else{
+        this.currentPlayer = this.currentPlayer == this.playerX ? this.playerO : this.playerX;
+      }
+    },
+    checkWinner(){
+      for(let i = 0; i<this.combinations.length; i++){
+        const [a, b, c] = this.combinations[i];
+        // Check if element not null and if player has won
+        if(this.board[a[0]][a[1]] != ''){
+          if(this.board[a[0]][a[1]] == this.board[b[0]][b[1]] && this.board[a[0]][a[1]] == this.board[c[0]][c[1]]){
+            console.log(this.currentPlayer + " WINS");
+            return true;
+          }
+        }
+      }
+    },
+    restartGame(){
+      this.board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ];
+      this.currentPlayer = this.playerX;
+      this.hasWinner = false;
     }
-  }
+  },
 };
 </script>
 
 <style>
-
+  .game_table {
+    display: grid;
+    justify-content: center;
+    grid-template: 100px 100px 100px / 100px 100px 100px;
+  }
+  button {
+    background-color: rgb(10, 102, 194);
+    border: none;
+    padding: 15px 0px;
+    width: 100px;
+    border-radius: 20px;
+    cursor: pointer;
+    margin-top: 20px;
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    font-size: 15px;
+    transition: background-color 0.15s;
+  }
+  button:hover{
+    background-color: rgb(2, 70, 138);
+  }
 </style>
